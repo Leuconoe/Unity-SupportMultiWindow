1f38a3f35b8e84f392e5d65b9423db6da855bbd5
Leuconoe <n.leuconoe@gmail.com>
2019-02-15 16:23:00 +0900

    first commit

---
+++ Assets/rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+    private Vector3 rot;
+	// Use this for initialization
+	void Start () {
+        rot = new Vector3();
+        rot.x = Random.Range(-2, 2);
+        rot.y = Random.Range(-2, 2);
+        rot.z = Random.Range(-2, 2);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rot, Time.deltaTime * 100);
+    }
+}
